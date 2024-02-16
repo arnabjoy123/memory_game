17300ec5dee9fc1ef01770a956b5e16648b284d9
arnabjoy123 <arnab99joy@gmail.com>
2024-02-16 12:30:33 +0530

    6th commit

--- index.js
+++ index.js
@@ -57,3 +57,18 @@ function resetBoard() {
 })();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
+
+var timeLeft = 30;
+var elem = document.getElementById("score-el");
+
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == -1) {
+    clearTimeout(timerId);
+    doSomething();
+  } else {
+    elem.innerHTML = timeLeft + " seconds remaining";
+    timeLeft--;
+  }
+}
